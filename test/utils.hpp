4243f1971002f906700edcfd7c34b3a989be48d3
Thomas Nixon <tom@tomn.co.uk>
2022-03-12 18:36:02 +0000

    make expected_len optional

--- test/utils.hpp
+++ test/utils.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include "cerealise/cerealise.hpp"
+#include <optional>
 
-template <typename T> void check_parse_unparse(T v1, size_t expected_len) {
+template <typename T>
+void check_parse_unparse(T v1, std::optional<size_t> expected_len = {}) {
   uint8_t buf[100];
   size_t len;
   REQUIRE(cerealise::unparse(v1, buf, 100, len));
-  REQUIRE(len == expected_len);
+  if (expected_len)
+    REQUIRE(len == *expected_len);
 
   T v2;
   REQUIRE(cerealise::parse(v2, buf, len));
