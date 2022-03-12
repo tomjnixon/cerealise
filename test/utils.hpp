bcff143ca38a1772e2e1746dc2dec4991e99b1bd
Thomas Nixon <tom@tomn.co.uk>
2022-03-12 23:16:58 +0000

    use vector for test buffer with correct size

--- test/utils.hpp
+++ test/utils.hpp
@@ -1,18 +1,20 @@
 #pragma once
 #include "cerealise/cerealise.hpp"
 #include <optional>
+#include <vector>
 
 template <typename T>
 void check_parse_unparse(T v1, std::optional<size_t> expected_len = {}) {
-  uint8_t buf[100];
+  std::vector<uint8_t> buf(expected_len.value_or(100));
+
   size_t len;
-  REQUIRE(cerealise::unparse(v1, buf, 100, len));
+  REQUIRE(cerealise::unparse(v1, buf.data(), buf.size(), len));
   if (expected_len)
     REQUIRE(len == *expected_len);
 
   T v2;
   size_t bytes_read;
-  REQUIRE(cerealise::parse(v2, buf, len, bytes_read));
+  REQUIRE(cerealise::parse(v2, buf.data(), len, bytes_read));
   REQUIRE(bytes_read == len);
 
   REQUIRE(v1 == v2);
